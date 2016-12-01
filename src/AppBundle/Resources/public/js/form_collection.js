3267a634db33edc9937296892084144ad507fc1f
lutras debtra <stuy.bradley@gmail.com>
2016-12-02 11:27:15 +1300

    Changed FormCollection.js so that it uses .prev('ul'), heavily simplifing the logic of the addNew buttons.

--- src/AppBundle/Resources/public/js/form_collection.js
+++ src/AppBundle/Resources/public/js/form_collection.js
@@ -33,21 +33,7 @@ function FormCollections(collection_classes) {
 
             // Add addNew events to every Add button the page.
             $(".btn-"+ val).each(function () {
-                // If the button is top level, they'll only be one UL so buttons
-                // can be placed outside it and the UL can be hardcoded.
-                if (val == "omics_experiment_types") {
-                    var parent_ul = $("ul.col-omics_experiment_types").first();
-                } else if (val == "statuses") {
-                    var parent_ul = $("ul.col-statuses").first();
-                    // Nested relations require automated search.
-                } else {
-                    // Handles Sequence Run case for samples being top-level.
-                    if (collection_classes.length == 1) {
-                        var parent_ul = $("ul.col-samples").first()
-                    } else {
-                        var parent_ul = $(this).closest('ul');
-                    }
-                }
+                var parent_ul = $(this).prev('ul');
                 var div_name = val;
                 $(this).on("click", function(e) {
                     e.preventDefault();
