b0242a90cf08a1da940490673b453abf454f5e74
lutras debtra <stuy.bradley@gmail.com>
2017-03-03 14:33:40 +1300

    Added create test to sequence_run including a sample.

--- tests/AppBundle/Controller/SequenceRunControllerTest.php
+++ tests/AppBundle/Controller/SequenceRunControllerTest.php
@@ -46,4 +46,48 @@ class SequenceRunControllerTest extends WebTestCase
         $this->assertEquals(2, $crawler->filter('strong:contains("Material")')->count());
         $this->assertEquals(2, $crawler->filter('p:contains("DNA")')->count());
     }
+
+    public function testCreate()
+    {
+        $this->loadTestFixtures();
+
+        $client = $this->makeClient();
+        $crawler = $client->request('POST', "/sequence_run/new");
+
+        $form = $crawler->selectButton("Create Sequence Run")->form();
+        $values = $form->getPhpValues();
+
+        $values['sequence_run']['runBy'] = 'stuart.bradley';
+        $values['sequence_run']['kit'] = "Illumina";
+        $values['sequence_run']['materialTypeString'] = 1;
+        $values['sequence_run']['runCoverageTarget'] = 10;
+        $values['sequence_run']['readLength'] = 10;
+
+        $values['sequence_run']['samples'] = [];
+        $values['sequence_run']['samples'][0] = [];
+
+        $values['sequence_run']['samples'][0]['BCExperimentID'] = 10;
+        $values['sequence_run']['samples'][0]['BCSampleID'] = 10;
+        $values['sequence_run']['samples'][0]['BCRunID'] = 10;
+        $values['sequence_run']['samples'][0]['sampledBy'] = 10;
+        $values['sequence_run']['samples'][0]['sampledBy'] = 10;
+        $values['sequence_run']['samples'][0]['materialTypeString'] = 1;
+        $values['sequence_run']['samples'][0]['RNALaterTreated'] = 1;
+
+        $values['sequence_run']['samples'][0]['sampledDateTime'] = [];
+        $values['sequence_run']['samples'][0]['sampledDateTime']['time'] = [];
+        $values['sequence_run']['samples'][0]['sampledDateTime']['date'] = [];
+
+        $values['sequence_run']['samples'][0]['sampledDateTime']['date']['year'] = 2012;
+        $values['sequence_run']['samples'][0]['sampledDateTime']['date']['month'] = 1;
+        $values['sequence_run']['samples'][0]['sampledDateTime']['date']['day'] = 1;
+
+        $values['sequence_run']['samples'][0]['sampledDateTime']['time']['hour'] = 00;
+        $values['sequence_run']['samples'][0]['sampledDateTime']['time']['minute'] = 00;
+
+        $crawler = $client->request($form->getMethod(), $form->getUri(), $values,
+            $form->getPhpFiles());
+        $crawler = $client->followRedirect();
+        $this->assertEquals(3, $crawler->filter('tr')->count());
+    }
 }
