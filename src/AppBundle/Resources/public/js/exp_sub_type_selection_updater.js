5ecf9269b5170b167d2a3b550dae0b9a6aba4077
lutras debtra <stuy.bradley@gmail.com>
2017-03-08 22:00:03 +1300

    Sub type selection now update to correct values on creation.

--- src/AppBundle/Resources/public/js/exp_sub_type_selection_updater.js
+++ src/AppBundle/Resources/public/js/exp_sub_type_selection_updater.js
@@ -4,6 +4,28 @@ function ExpSubTypeSelectionUpdater(selection_relations) {
         $('ul.col-omics_experiment_types').on('change', 'select[id$="omicsExperimentTypeString"]', function (e) {
             self.modify(e.target);
         });
+
+        // Observes new sub_type additions and updates them accordingly.
+        var observer = new MutationObserver(function(mutations) {
+            mutations.forEach(function(mutation) {
+                var nodes = mutation.addedNodes[0];
+                var sub_type_select = $(nodes).find("select[id$='omicsExperimentSubTypeString']");
+                if ($(sub_type_select).length) {
+                    var sub_type_select_id = $(sub_type_select).attr('id').match(/^omics_experiment_omicsExperimentTypes_(\d+)/)[1];
+                    var type_select = $('select#omics_experiment_omicsExperimentTypes_' + sub_type_select_id + '_omicsExperimentTypeString');
+                    var parent_val = $("option:selected", type_select).text();
+                    self.update_child(sub_type_select, parent_val);
+                }
+
+            });
+        });
+
+        var observerConfig = {
+            subtree: true,
+            childList: true
+        };
+
+        observer.observe($('ul.col-omics_experiment_types')[0], observerConfig);
     };
 
     self.modify = function(selector) {
@@ -12,26 +34,29 @@ function ExpSubTypeSelectionUpdater(selection_relations) {
         // Finds experiment_sub_type select children.
         var child_selectors = $(frame).find("select[id$='omicsExperimentSubTypeString']");
         $.each(child_selectors, function (i, val) {
-            var child_selector = val;
-            // Hides ALL options.
-            $(child_selector).children('option').hide();
-            // Shows relevant options.
-            $.each(selection_relations[parent_val], function (i, val) {
-                $(child_selector).children('option').filter(function () {
-                    return $(this).html() == val;
-                }).show();
-            });
-            // Gets first non-hidden option.
-            var first_non_hidden_option = $(child_selector).children('option').filter(function () {
-                return $(this).css('display') != 'none';
-            }).first()[0];
-            // If it exists set it as selected, else set blank.
-            if (typeof(first_non_hidden_option) != 'undefined') {
-                $(child_selector).val(first_non_hidden_option.value);
-            } else {
-                $(child_selector).val('');
-            }
+            self.update_child(val, parent_val);
         });
+    };
+
+    self.update_child = function(child_selector, parent_val) {
+        // Hides ALL options.
+        $(child_selector).children('option').hide();
+        // Shows relevant options.
+        $.each(selection_relations[parent_val], function (i, val) {
+            $(child_selector).children('option').filter(function () {
+                return $(this).html() == val;
+            }).show();
+        });
+        // Gets first non-hidden option.
+        var first_non_hidden_option = $(child_selector).children('option').filter(function () {
+            return $(this).css('display') != 'none';
+        }).first()[0];
+        // If it exists set it as selected, else set blank.
+        if (typeof(first_non_hidden_option) != 'undefined') {
+            $(child_selector).val(first_non_hidden_option.value);
+        } else {
+            $(child_selector).val('');
+        }
     };
 
     self.construct();
