524bbdcf660711cff1da8fd93cf8cc5471791c1e
lutrasdebtra <stuy.bradley@gmail.com>
2017-12-16 10:29:44 +1300

    Fixed Web Assets.

---
+++ web/js/summernote_config.js
@@ -0,0 +1,26 @@
+/**
+ * Created by wackm on 19-Mar-17.
+ */
+$(document).ready(function () {
+    $('.summernote').each(function () {
+        $(this).summernote({
+            // Pops image data if loaded.
+            callbacks: {
+                onImageUpload: function (data) {
+                    data.pop();
+                }
+            },
+            // Removes image and video uploading.
+            toolbar: [
+                ['style', ['style']],
+                ['style_text', ['bold', 'italic', 'underline', 'clear']],
+                ['font', ['fontname', 'fontsize']],
+                ['color', ['color']],
+                ['para', ['ul', 'ol', 'paragraph']],
+                ['insert_table', ['table']],
+                ['link', ['link']],
+                ['misc', ['fullscreen', 'codeview', 'help']]
+            ]
+        });
+    });
+});
